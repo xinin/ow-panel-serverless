d24bc317b7951eddd14a5e0752e4a8d72e623179
Diego Prieto <diego.prieto@beeva.com>
2017-09-01 11:21:05 +0200

    avanzando logs, no probado

--- project/handler.js
+++ project/handler.js
@@ -4,18 +4,24 @@ const appLib = require(__dirname + '/lib/App');
 const App = new appLib();
 
 
-module.exports.hello = (event, context, callback) => {
+
+module.exports.hello = (req, context, callback) => {
+
+    const Middleware = App.Middleware();
+    Middleware.exec(req);
 
     const Cloud = App.Cloud();
 
-    Cloud.Log.info(null, 'Escribiendo info CloudWatch');
-    Cloud.Log.error(null, 'Escribiendo error CloudWatch');
+    Cloud.Log.info(req, 'Escribiendo info CloudWatch');
+    Cloud.Log.info(req, 'Escribiendo 2 info CloudWatch');
+
+    Cloud.Log.error(req, 'Escribiendo error CloudWatch');
 
     const response = {
         statusCode: 200,
         body: JSON.stringify({
             message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: event,
+            input: req,
         }),
     };
 
