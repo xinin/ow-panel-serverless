a1efbdda59adff9a28a6b4d9fcd8544fc344512f
Diego Prieto <diego.prieto@beeva.com>
2017-09-04 10:24:56 +0200

    algo falla en el logger

--- project/handler.js
+++ project/handler.js
@@ -4,30 +4,51 @@ const appLib = require(__dirname + '/lib/App');
 const App = new appLib();
 
 
-
 module.exports.hello = (req, context, callback) => {
+
+    //TODO ver como hacer un wrapper general para englobar esto aunque sea azure function u otra casa
 
     const Middleware = App.Middleware();
     Middleware.exec(req);
 
     const Cloud = App.Cloud();
+    // Cloud.Log.info(req, 'Escribiendo info CloudWatch');
+    // Cloud.Log.info(req, 'Escribiendo 2 info CloudWatch');
+    // Cloud.Log.info(req, {a: 'pepito', b: { c: 1, d: 'tururu'}});
 
-    Cloud.Log.info(req, 'Escribiendo info CloudWatch');
-    Cloud.Log.info(req, 'Escribiendo 2 info CloudWatch');
 
-    Cloud.Log.error(req, 'Escribiendo error CloudWatch');
+    // Cloud.Log.error(req, 'Escribiendo ERROR CloudWatch');
+
+    //App.Test.Log.info("PRUEBA");
+
+
+    /*
+    const Project = App.Model('Project');
+    Project.get(req, 'projectId').then(
+        data => {
+
+        }, err => {
+
+        }
+    );
+*/
+
+    console.log("ANTES DE CONSOLE");
 
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: req,
+            message: 'Go Serverless v1.0! Your function executed successfully!'
         }),
     };
 
 
+    console.log("ANTES DE CALLBACK")
     callback(null, response);
 
+    console.log("CALLBACK OK")
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     //callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+
